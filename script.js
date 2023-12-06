d084da412046c1bf9df7872d6ac56d279ff3f2f3
coder-txane <92263760+coder-txane@users.noreply.github.com>
2023-12-06 19:15:17 +0000

    Fix the inputs and issues linked to calculator

--- script.js
+++ script.js
@@ -10,89 +10,87 @@ Algorithm
 // Elements
 
 const form = document.getElementById('wrapper');
-
 const bill = document.getElementById('bill');
 const billError = document.getElementById('first-error');
-
-const custom = document.getElementById('custom');
 const people = document.getElementById('people');
 const tipError = document.getElementById('second-error');
-
 const tipAmount = document.getElementById('tip-amount');
 const totalCost = document.getElementById('total-cost');
-
+const custom = document.getElementById('custom');
+const tip = document.getElementsByName('radio-btn');
 const button = document.getElementById('reset-btn');
 
 // Calculator
 
 form.addEventListener('submit', (e) => {
-
     if (!e.target.checkValidity()) {
         // form is invalid - cancel submit
         e.preventDefault();
         e.stopImmediatePropagation();
     }
-
-    if (bill.value === '0' || !bill.value) {
-        billError.style.visibility = 'visible';
-        bill.style.border = '2px solid rgb(237, 109, 109)';
-    } else {
-        billError.style.visibility = 'hidden';
-        bill.style.border = 'none';
-        bill.value = '';
-    }
-
-    if (people.value === '0' || !people.value) {
-        tipError.style.visibility = 'visible';
-        people.style.border = '2px solid rgb(237, 109, 109)';
-    } else {
-        tipError.style.visibility = 'hidden';
-        people.style.border = 'none';
-        people.value = '';
-    }
 });
 
+// Valid and invalid styling for reset button
 form.addEventListener('input', () => {
-    if (bill.value && people.value) {
-        button.style.opacity = '1';
-        button.style.cursor = 'pointer';
-        button.removeAttribute('disabled');
-    } else if (!bill.value || !people.value) {
-        button.style.opacity = '0.3';
-        button.style.cursor = 'not-allowed';
-        //button.addAttribute('disabled');
-    }
-
-    getTipAmount();
+    updateResetButton();
+    updateTotals();
 });
 
-// Calculations
+// Event listener for custom input
+custom.addEventListener('input', () => {
+    if (custom.validity.valid) {
+        // Add the class for green border
+        custom.style.border = '2px solid hsl(172, 67%, 45%)';
+    } else {
+        // Remove the class if the input value is not valid
+        custom.style.border = '2px solid rgb(237, 109, 109)';
+    }
+
+    updateTotals();
+});
+
+custom.addEventListener('click', () => {
+    // Uncheck the other tip buttons
+    uncheckTipButtons();
+});
+
+tip.forEach(button => {
+    button.addEventListener('input', () => {
+        updateTotals();
+    })
+})
+
+function updateResetButton() {
+    button.style.opacity = bill.value && people.value ? '1' : '0.3';
+    button.style.cursor = bill.value && people.value ? 'pointer' : 'not-allowed';
+    button.removeAttribute('disabled');
+}
+
+function uncheckTipButtons() {
+    tip.forEach(button => {
+        button.checked = false;
+    })
+}
 
 // Tip percentage
-function getTipAmount() {
-    const tip = document.getElementsByName('radio-btn');
-    let tipPercentage = 0;
+function updateTotals() {
+    const billValue = parseFloat(bill.value);
+    const peopleValue = parseFloat(people.value);
 
-    for (let i = 0; i < tip.length; i++) {
-        if (tip[i].checked) {
-            tipPercentage = parseFloat(tip[i].value) / 100; // Convert percentage to decimal
-
-            // Check if bill.value and people.value are not empty and not zero before performing the division
-            const billValue = parseFloat(bill.value);
-            const peopleValue = parseFloat(people.value);
-
-            if (!isNaN(billValue) && !isNaN(peopleValue) && peopleValue !== 0) {
-                const tipAmountValue = billValue * tipPercentage / peopleValue;
-                const totalCostValue = billValue / peopleValue + tipAmountValue;
-                
-                // Set the tip amount content
-                tipAmount.textContent = tipAmountValue.toFixed(2);
-                totalCost.textContent = totalCostValue.toFixed(2);
-            } else {
-                // Handle the case when either bill.value or people.value is empty or zero
-                tipAmount.textContent = '0';
-                totalCost.textContent = '0';
-            }
-        }
+    // Handle the case when either bill.value or people.value is empty or zero
+    if (isNaN(billValue) || isNaN(peopleValue) || peopleValue === 0) {
+        tipAmount.textContent = '0';
+        totalCost.textContent = '0';
+        return;
     }
+
+    const selectedTipButton = Array.from(tip).find(button => button.checked);
+    const tipPercentage = selectedTipButton ? parseFloat(selectedTipButton.value) / 100 : parseFloat(custom.value) / 100;
+
+    const tipAmountValue = billValue * tipPercentage / peopleValue;
+    const totalCostValue = billValue / peopleValue + tipAmountValue;
+
+    // Set the tip amount content
+    tipAmount.textContent = tipAmountValue.toFixed(2);
+    totalCost.textContent = totalCostValue.toFixed(2);
 }
